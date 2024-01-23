553dc0fb714a0eb4f1d9f729e7b9944bec156600
tahariabegum <taharia97@gmail.com>
2024-01-22 20:40:10 -0500

    try/catch to config added

--- config.js
+++ config.js
@@ -1,7 +1,11 @@
 const mongoose = require ('mongoose') 
 const mongoConfig = async() => {
-    const result = await mongoose.connect(process.env.MONGO_URL)
-    console.log('database connected', result.connection.host)
+    try{
+        const result = await mongoose.connect(process.env.MONGO_URL)
+        console.log('database connected', result.connection.host)
+    } catch (err) {
+        console.log('Mongo Error', err)
+    }
 }
 
 module.exports = mongoConfig 
