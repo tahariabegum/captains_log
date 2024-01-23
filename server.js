b6b69e4843ec0e51dc5def5a0ceb688ba98d62bf
tahariabegum <taharia97@gmail.com>
2024-01-22 20:58:32 -0500

    change from on to true and added mongoose

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require ('express')
+
+require ('dotenv').config()
 const mongoConfig = require ('./config')
 
 const app = express()
@@ -11,15 +13,24 @@ app.engine('jsx', jsxEngine());
 
 app.use(express.urlencoded({extended:false}))
 
+app.get('/' , (req,res) => {
+    res.send('test')
+})
+
 app.get('/logs/new', (req, res) => {
     res.render('New')
 })
 
 app.post('/logs', (req, res) => {
-    res.send("received")
+    res.send(req.body)
+    if (req.body.shipIsBroken === 'on') {
+        req.body.shipIsBroken = true
+    } else {
+        req.body.shipIsBroken = false
+    }
 })
 
 app.listen(PORT, () => {
     console.log('Listening on port: ' + PORT, process.env.MONGO_URL)
-    mongoconfig()
+    mongoConfig()
 })
